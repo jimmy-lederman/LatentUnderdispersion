7dd442e4f6b12fea88cb45db005437b78a4104f0
James Robert Lederman <jlederman@fe03>
2026-02-15 21:11:20 -0600

    lil

--- realdata/flights/runheldoutrevision.jl
+++ realdata/flights/runheldoutrevision.jl
@@ -14,7 +14,7 @@ g = parse(Int, ARGS[5])
 T = 99 #because running on full
 #thread = Bool(parse(Int, ARGS[1]))
 
-datafile = "/home/jlederman/DiscreteOrderStatistics/data/airtime.csv"
+datafile = "/home/jlederman/DiscreteOrderStatistics/data/flights_data/flights_final.csv"
 df = CSV.read(datafile, DataFrame)
 df = select(df, Not(1))
 Y_NM = convert(Matrix{Int}, Matrix(select(df, 1)))
@@ -37,11 +37,10 @@ for t1 in 1:T
             indices = findall(bitvector)
             distances = dist_NM[indices, 1]
             @assert all(x -> x == distances[1], distances) "Not all distances are equal"
-            push!(routes_R4, [t1,t2,indices, distances[1]])
+            push!(routes_R4, [t1,t2,indices, distances[1],Y_NM[indices,1],length(indices)])
         end
     end
 end
-Matrix(routes_R4')
 routes_R4 = Matrix(hcat(routes_R4...)')
 #println(routes_R4)
 routes_N = zeros(Int, N)
@@ -57,14 +56,14 @@ R = size(routes_R4)[1]
 
 N = size(Y_NM)[1]
 M = size(Y_NM)[2]
-a =20
-b =100
-c =10
-d=1
+a =1
+b =.01
+# c =10
+# d=1
 alpha = beta = 1 #not used
 tau2 = 50^2
-gforwards = [x -> x, x -> x^2]
-gbakwards = [x -> x, sqrt]
+gforwards = [x -> x, sqrt]
+gbakwards = [x -> x, x -> x^2]
 
 Nsamples = 500
 nbunrin = 1000
@@ -75,9 +74,9 @@ Random.seed!(maskSeed)
 mask_NM = rand(N, M) .< .05
 
 #need to format things such that the model doesn't see the test data at all
-info = Dict("routes_R4"=>routes_R4, "I_N3"=>I_N3, "dist_N"=>dist_NM)
 Ntest = sum(mask_NM)
 Ntrain = N - Ntest
+routes_R2 = routes_R4[:,3:6]
 datatrain = Dict("Y_NM"=>reshape(data["Y_NM"][mask_NM .== 0],:,1), "routes_R2"=>routes_R2, "routes_N"=>routes_N[mask_NM[:,1] .== 0])
 #datatest = Dict("Y_NM"=>reshape(data["Y_NM"][mask_NM .== 1],:,1), "routes_R2"=>routes_R2, "routes_N"=>routes_N[mask_NM[:,1] .== 1])
 routes_R2train = copy(routes_R2)
@@ -86,16 +85,17 @@ for r in 1:R
     routes_R2train[r,2] = length(findall(datatrain["routes_N"] .== r))
 end
 datatrain = Dict("Y_NM"=>reshape(data["Y_NM"][mask_NM .== 0],:,1), "routes_R2"=>routes_R2train, "routes_N"=>routes_N[mask_NM[:,1] .== 0])
-
+info = Dict("routes_R2"=>routes_R2, "routes_N"=>routes_N[mask_NM[:,1] .== 0])
 if type == 1
-    include("/home/jlederman/DiscreteOrderStatistics/models/flights/flights_final.jl")
-    model = flights(Ntrain, M, T, R, Dmax, a, b, c, d, alpha, beta)
-    if D > 0
-        t = @elapsed samples = fit(model, data, nsamples = Nsamples, nburnin=nbunrin, nthin=nthin, mask=nothing, info=info, initseed=chainSeed,
+    Dmax = 9
+    include("/home/jlederman/DiscreteOrderStatistics/models/flights_final/flights.jl")
+    model = flights(Ntrain, M, R, Dmax, a, b, alpha, beta)
+    if D == 0
+        t = @elapsed samples = fit(model, datatrain, nsamples = Nsamples, nburnin=nbunrin, nthin=nthin, mask=nothing, info=info, initseed=chainSeed,
         verbose =true)
         samplesnew = [Dict("U_R"=> s["U_R"], "D_R" => s["D_R"]) for s in samples]
-    else
-        t = @elapsed samples = fit(model, data, nsamples = Nsamples, nburnin=nbunrin, nthin=nthin, mask=nothing, info=info, initseed=chainSeed,
+    elseif D > 0
+        t = @elapsed samples = fit(model, datatrain, nsamples = Nsamples, nburnin=nbunrin, nthin=nthin, mask=nothing, info=info, initseed=chainSeed,
         constantinit=Dict("D_R"=>fill(D,R)),skipupdatealways=["D_R"], verbose =true)
         samplesnew = [Dict("U_R"=> s["U_R"], "D_R" => s["D_R"]) for s in samples]
     end
@@ -103,16 +103,16 @@ elseif type == 2
     gforward = gforwards[g]
     gbakward = gbakwards[g]
     include("/home/jlederman/DiscreteOrderStatistics/models/flights_final/flights_STAR.jl")
-    model = flights_STAR(Ntrain, M, R, alpha, beta, tau2, g1, g_inv1)
-    t = @elapsed samples = fit(model, data, nsamples = Nsamples, nburnin=nbunrin, nthin=nthin, mask=nothing, info=info, initseed=chainSeed,
+    model = flights_STAR(Ntrain, M, R, alpha, beta, tau2, gforward, gbakward)
+    t = @elapsed samples = fit(model, datatrain, nsamples = Nsamples, nburnin=nbunrin, nthin=nthin, mask=nothing, info=info, initseed=chainSeed,
     verbose =true)
-    samplesnew = [Dict("U_R"=> s["U_R"], "sigma2_R" => s["Dsigma2_RR"]) for s in samples]
+    samplesnew = [Dict("U_R"=> s["U_R"], "sigma2_R" => s["sigma2_R"]) for s in samples]
 end
 
 params = [maskSeed,chainSeed,type,D,g]
 folder = "/net/projects/schein-lab/jimmy/OrderStats/realdata/flights/"
 if type == 1
-    save(folder*"revisionsamples/MedPoissonD$(D)mask$(maskSeed)chain$(chainSeed).jld", "params", params, "samples", samplesnew, "mask" => mask_NM, "time", t)
+    save(folder*"revisionsamples/MedPoissonD$(D)mask$(maskSeed)chain$(chainSeed).jld", "params", params, "samples", samplesnew, "mask", mask_NM, "time", t)
 elseif type == 2
-    save(folder*"revisionsamples/STARg$(g)mask$(maskSeed)chain$(chainSeed).jld", "params", params, "samples", samplesnew, "mask" => mask_NM, "time", t)
+    save(folder*"revisionsamples/STARg$(g)mask$(maskSeed)chain$(chainSeed).jld", "params", params, "samples", samplesnew, "mask", mask_NM, "time", t)
 end
